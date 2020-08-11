4feee8c70a0ca6e75d8a0e6582f8dae54ea7b219
Takatoshi MATSUO <matsuo.tak@gmail.com>
2020-08-11 14:15:14 +0900

    delete vnc password auth because of using nginx pam auth

--- src/common/scripts/vnc_startup.sh
+++ src/common/scripts/vnc_startup.sh
@@ -69,20 +69,20 @@ $STARTUPDIR/chrome-init.sh
 VNC_IP=$(hostname -i)
 
 ## change vnc password
-echo -e "\n------------------ change VNC password  ------------------"
+#echo -e "\n------------------ change VNC password  ------------------"
 # first entry is control, second is view (if only one is valid for both)
 mkdir -p "$HOME/.vnc"
-PASSWD_PATH="$HOME/.vnc/passwd"
 
-if [ ! -f $PASSWD_PATH ]; then
-    if [[ $VNC_VIEW_ONLY == "true" ]]; then
-        echo "start VNC server in VIEW ONLY mode!"
-        #create random pw to prevent access
-        echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
-    fi
-    echo "$PASSWORD" | vncpasswd -f >> $PASSWD_PATH
-    chmod 600 $PASSWD_PATH
-fi
+#PASSWD_PATH="$HOME/.vnc/passwd"
+#if [ ! -f $PASSWD_PATH ]; then
+#    if [[ $VNC_VIEW_ONLY == "true" ]]; then
+#        echo "start VNC server in VIEW ONLY mode!"
+#        #create random pw to prevent access
+#        echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
+#    fi
+#    echo "$PASSWORD" | vncpasswd -f >> $PASSWD_PATH
+#    chmod 600 $PASSWD_PATH
+#fi
 
 ## start vncserver and noVNC webclient
 echo -e "\n------------------ start noVNC  ----------------------------"
@@ -97,8 +97,8 @@ vncserver -kill $DISPLAY &> $STARTUPDIR/vnc_startup.log \
     || echo "no locks present"
 
 echo -e "start vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
-if [[ $DEBUG == true ]]; then echo "vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION"; fi
-vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry 1280x1024 -localhost -nolisten tcp &> $STARTUPDIR/no_vnc_startup.log
+if [[ $DEBUG == true ]]; then echo "vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry 1280x1024 -localhost -nolisten tcp -SecurityTypes=None"; fi
+vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry 1280x1024 -localhost -nolisten tcp -SecurityTypes=None &> $STARTUPDIR/no_vnc_startup.log
 
 echo -e "start window manager\n..."
 $HOME/wm_startup.sh &> $STARTUPDIR/wm_startup.log
